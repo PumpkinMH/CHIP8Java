86f45e004344fb265258aed00e0f66330640dd26
Mabel <mahurd@email.sc.edu>
2024-12-11 14:02:31 -0500

    Added switch to update the keypad array

--- KeyboardListener.java
+++ KeyboardListener.java
@@ -15,13 +15,19 @@ public class KeyboardListener implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         System.out.println(e.getKeyChar());
-
-
+        Character keypadChar;
+        if((keypadChar = keypadMapping.get(e.getKeyChar())) != null) {
+            updateKeys(true, keypadChar);
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-//        System.out.println(e.getKeyChar());
+        System.out.println(e.getKeyChar());
+        Character keypadChar;
+        if((keypadChar = keypadMapping.get(e.getKeyChar())) != null) {
+            updateKeys(false, keypadChar);
+        }
     }
 
     public KeyboardListener() {
@@ -48,7 +54,56 @@ public class KeyboardListener implements KeyListener {
     }
 
     private void updateKeys(boolean pressed, char key) {
-
+        switch (key) {
+            case '0':
+                keypad[0x0] = pressed;
+                break;
+            case '1':
+                keypad[0x1] = pressed;
+                break;
+            case '2':
+                keypad[0x2] = pressed;
+                break;
+            case '3':
+                keypad[0x3] = pressed;
+                break;
+            case '4':
+                keypad[0x4] = pressed;
+                break;
+            case '5':
+                keypad[0x5] = pressed;
+                break;
+            case '6':
+                keypad[0x6] = pressed;
+                break;
+            case '7':
+                keypad[0x7] = pressed;
+                break;
+            case '8':
+                keypad[0x8] = pressed;
+                break;
+            case '9':
+                keypad[0x9] = pressed;
+                break;
+            case 'A':
+                keypad[0xA] = pressed;
+                break;
+            case 'B':
+                keypad[0xB] = pressed;
+                break;
+            case 'C':
+                keypad[0xC] = pressed;
+                break;
+            case 'D':
+                keypad[0xD] = pressed;
+                break;
+            case 'E':
+                keypad[0xE] = pressed;
+                break;
+            case 'F':
+                keypad[0xF] = pressed;
+                break;
+        }
     }
 
     public boolean[] getStaticKeys() {
